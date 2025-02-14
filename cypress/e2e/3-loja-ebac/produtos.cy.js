83c1762a62115221c5072246cd31713acd0d2f74
Sergio Oliveira <sergiofo87@gmail.com>
2025-02-13 21:21:21 -0300

    Page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,29 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
 
 beforeEach(() => {
-    cy.visit('produtos')
+    produtosPage.visitarUrl()
 });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(4)
-            .contains('Aero Daily Fitness Tee')
-            .click()
-
+        produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
             cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Teton Pullover Hoodie'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+
+    it('Deve  visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
