d68c9ca576a5db182a444087ef42032097c06b3a
Sergio Oliveira <sergiofo87@gmail.com>
2025-02-17 21:28:21 -0300

    Adicionando Produtos no Carrinho

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -3,7 +3,7 @@
 import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
-
+})
 beforeEach(() => {
     produtosPage.visitarUrl()
 });
@@ -13,17 +13,34 @@ beforeEach(() => {
             cy.get('#tab-title-description > a').should('contain' , 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Teton Pullover Hoodie'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
 
     it('Deve  visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain' , 'Aether Gym Pant')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
-        
+        let qtd = 7    
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('XL' , 'Red' , qtd)
+
+        cy.get('.woocommerce-message').should('contain' , qtd + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
     });
-});
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => { 
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[1].tamanho, 
+                dados[1].cor, 
+                dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain' , dados[1].nomeProduto)
+        })
+        
+
+})
